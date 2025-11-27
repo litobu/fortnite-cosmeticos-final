d678f159f2e340cbd775d9633fff7edf8c42acc2
chrystopher <chrysbussines@gmail.com>
2025-11-27 17:15:00 -0300

    vite api.env

--- frontend/scripts/api/fortniteApi.js
+++ frontend/scripts/api/fortniteApi.js
@@ -1,6 +1,7 @@
 // Proxy para a rota do backend (/api/fortnite/store)
 export async function fetchStore(){
-  const res = await fetch('/https://backend-f6oa.onrender.comapi/fortnite/store');
+  const res = await fetch('https://backend-f6oa.onrender.com/api/fortnite/store');
+;
   if(!res.ok) throw new Error('Falha ao obter loja');
   const data = await res.json();
   return data.items ?? data;
